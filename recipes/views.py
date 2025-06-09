5365a86ade97039b104abc42de9d1218259b22da
Joao Paulo <joaopaulodamotalima@hotmail.com>
2025-06-09 14:07:16 -0300

    Adicionando recipes e rotas

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
